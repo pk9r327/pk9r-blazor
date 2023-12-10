0eb34c75b9c40fa9e51987ccbe9a048980fde5f3
La Minh Phuc <pk9r327@outlook.com>
2023-12-10 15:19:30 +0700

    [wip-lw] fluent-blazor demo layout
    The most significant changes involve the addition and removal of project references, updates to namespaces and `using` statements, and changes to layout and component files.
    
    1. The project references in `Pk9r.Blazor.Minesweeper.Client.csproj` and `Pk9r.Blazor.Minesweeper.Components.csproj` have been updated to include new components and layouts. This change allows these projects to access and use the newly added components and layouts.
    
    2. The `using` statements in `_Imports.razor` have been updated to include new namespaces, which allows the use of classes and interfaces from these namespaces without having to fully qualify their names.
    
    3. The layout in `MainLayout.razor` has been updated to use a new layout, `Pk9rFluentBlazorDemoLayout`, which changes the overall look and feel of the application.
    
    4. The `div` class in `Component1.razor` has been updated to inherit from `FluentComponentBase`, which provides a base for creating new components that use the Fluent UI design system.
    
    5. The namespace in `RenderMode.cs` has been updated to `Pk9r.Blazor.Components`, which changes the scope of the `RenderMode` class.
    
    6. Two project files, `Pk9r.Blazor.csproj` and `Pk9r.Blazor.Layouts.FluentBlazorDemo.csproj`, have been removed, which means these projects are no longer part of the solution.
    
    7. Two new project files, `Pk9r.Blazor.Components.csproj` and `Pk9r.Blazor.Layouts.FluentBlazorDemo.Components.csproj`, have been added, which means these projects are now part of the solution.
    
    8. The `Pk9rFluentBlazorDemoLayout.razor` and `Pk9rFluentBlazorDemoLayout.razor.css` files have been added, which provide a new layout and associated styles for the application.
    
    9. The `Component1.razor` and `Component1.razor.css` files have been removed, which means this component and its styles are no longer part of the application.
    
    10. The `ExampleJsInterop.cs` and `exampleJsInterop.js` files have been removed, which means these JavaScript interop files are no longer part of the application.
    
    11. The `_Imports.razor` files have been updated to include `Microsoft.FluentUI.AspNetCore.Components` and `Microsoft.FluentUI.AspNetCore.Components.Utilities`, which allows the use of classes and interfaces from these namespaces without having to fully qualify their names.

--- src/Core/RenderMode/RenderMode.cs
+++ src/Core/RenderMode/RenderMode.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.AspNetCore.Components.Web;
 
-namespace Pk9r.Blazor;
+namespace Pk9r.Blazor.Components;
 public static class RenderMode
 {
     public static InteractiveServerRenderMode InteractiveServerWithoutPrerendering { get; } = new InteractiveServerRenderMode(prerender: false);
